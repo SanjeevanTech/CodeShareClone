6b402b64e9852a201415cd031e010a0c3d157440
sanjeevantech2006@gmail.com <sanjeevantech2006@gmail.com>
2025-02-28 09:26:33 +0530

    update 6

--- src/page/home.jsx
+++ src/page/home.jsx
@@ -4,7 +4,7 @@ import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
 const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
 
-const socket = io('backendUrl');
+const socket = io(backendUrl);
 
 const Home = () => {
     const [code, setCode] = useState('');
